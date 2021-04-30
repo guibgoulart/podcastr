ee1158b8cd1a91d6b11032acb864ff009002a51f
unknown <gui.bgoulart@gmail.com>
2021-04-29 22:18:47 -0300

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import styles from '../styles/app.module.scss';
+import '../styles/global.scss';
+
+import { Player } from '../components/Player';
+import { Header } from '../components/Header';
+import React from 'react';
+import { PlayerContextProvider } from '../context/PlayerContext';
+
+function MyApp({ Component, pageProps }) {
+
+  return (
+    <PlayerContextProvider>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+      </PlayerContextProvider>
+  )
+}
+
+export default MyApp;
